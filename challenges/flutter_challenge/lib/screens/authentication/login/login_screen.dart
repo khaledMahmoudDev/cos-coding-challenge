72226f24b2849a60ef68e2cdc92b9f8f983b4648
khaled mahmoud <khaled.015067@ci.suez.edu.eg>
2022-05-24 00:45:08 +0200

    create flutter app

---
+++ challenges/flutter_challenge/lib/screens/authentication/login/login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LogINScreen extends StatefulWidget {
+  const LogINScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LogINScreen> createState() => _LogINScreenState();
+}
+
+class _LogINScreenState extends State<LogINScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
