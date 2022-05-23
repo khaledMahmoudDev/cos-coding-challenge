683f2a5e9ca0cd7c6723e42bc52000401cbf939b
khaled mahmoud <khaled.015067@ci.suez.edu.eg>
2022-05-24 01:56:22 +0200

    update welcome design

--- challenges/flutter_challenge/lib/screens/authentication/welcome_auth/welcome_auth.dart
+++ challenges/flutter_challenge/lib/screens/authentication/welcome_auth/welcome_auth.dart
@@ -12,16 +12,108 @@ class _WelcomeAuthScreenState extends State<WelcomeAuthScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage(CarOnSaleImages.mainBackground),
-                fit: BoxFit.fill),
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            color: const Color(0xFFFADF85),
           ),
-        ),
-      ],
-    ));
+          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Expanded(
+              flex: 5,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 100, left: 25),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'CAR',
+                      style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF474B57)),
+                    ),
+                    Text(
+                      'ON',
+                      style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF474B57)),
+                    ),
+                    Text(
+                      'SALE',
+                      style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF474B57)),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Image.asset(CarOnSaleImages.car),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Column(
+                children: [
+                  Container(
+                    height: 80,
+                    width: double.infinity,
+                    padding:
+                        const EdgeInsets.only(top: 25, left: 24, right: 24),
+                    child: MaterialButton(
+                      // onPressed: () => Navigator.of(context)
+                      //     .pushNamed(LoginScreen.routeName),
+                      onPressed: () {},
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      color: Colors.indigo,
+                      child: Text(
+                        'Log In',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 80,
+                    width: double.infinity,
+                    padding:
+                        const EdgeInsets.only(top: 25, left: 24, right: 24),
+                    child: MaterialButton(
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      color: Colors.white,
+                      onPressed: () {},
+                      // onPressed: () => Navigator.of(context)
+                      //     .pushNamed(SignupScreen.routeName),
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.lightBlue,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ])
+        ],
+      ),
+    );
   }
 }
--- challenges/flutter_challenge/lib/utils/images.dart
+++ challenges/flutter_challenge/lib/utils/images.dart
@@ -1,3 +1,4 @@
 class CarOnSaleImages {
   static const String mainBackground = "assets/images/main_background.jpg";
+  static const String car = "assets/images/car.png";
 }
